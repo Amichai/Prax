e887dd491c6fcd4ea96f177f749e686589fd6ea7
SLaks <Schabse@GMail.com>
2010-06-27 22:49:33 -0400

    DummyProcesor

--- Source/OcrEngine/Prax.OcrEngine.Common/IDocumentProcessor.cs
+++ Source/OcrEngine/Prax.OcrEngine.Common/IDocumentProcessor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Threading;
 
 namespace Prax.OcrEngine.Common {
 	///<summary>Performs OCR processing.</summary>
@@ -32,5 +33,39 @@ namespace Prax.OcrEngine.Common {
 		//TODO: Add classes or interfaces to Common
 		//that store the OCR's results, then expose
 		//them as properties in this interface.
+	}
+	public class DummyProcesor : IDocumentProcessor {
+		static readonly Random Rand = new Random();
+		///<summary>Does nothing for a while.</summary>
+		public void ProcessDocument(Stream document) {
+			IsProcessing = true;
+
+			MaximumProgress = Rand.Next(15, 45);
+			for (int i = 0; i < MaximumProgress; i++) {
+				Thread.Sleep(TimeSpan.FromSeconds(Rand.Next(30, 90)));
+				CurrentProgress = i + 1;
+				OnProgressChanged();
+			}
+
+			IsProcessing = false;
+		}
+		///<summary>Gets the current progress of the operation.</summary>
+		public int CurrentProgress { get; private set; }
+		///<summary>Gets the maximum progress of the operation.</summary>
+		///<remarks>If CurrentProgress is equal to MaximumProgress, the operation has finished.</remarks>
+		public int MaximumProgress { get; private set; }
+		///<summary>Indicates whether this instance is currently processing a document.</summary>
+		public bool IsProcessing { get; private set; }
+
+		///<summary>Occurs when the progress of the operation changes.</summary>
+		public event EventHandler ProgressChanged;
+		///<summary>Raises the ProgressChanged event.</summary>
+		internal protected virtual void OnProgressChanged() { OnProgressChanged(EventArgs.Empty); }
+		///<summary>Raises the ProgressChanged event.</summary>
+		///<param name="e">An EventArgs object that provides the event data.</param>
+		internal protected virtual void OnProgressChanged(EventArgs e) {
+			if (ProgressChanged != null)
+				ProgressChanged(this, e);
+		}
 	}
 }
