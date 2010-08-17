ce20d12ae1de860fc865110a66a7d2be9801ae6f
SLaks <Schabse@GMail.com>
2010-08-16 22:36:44 -0400

    Fix creation, doc comments

--- Source/OcrEngine/Prax.OcrEngine.Website/Content/Javascript/ProgressBar.js
+++ Source/OcrEngine/Prax.OcrEngine.Website/Content/Javascript/ProgressBar.js
@@ -1,53 +1,56 @@
 ﻿/// <reference path="jquery-1.4.1-vsdoc.js" />
-(function ($) {
-	function ProgressBar(container, outerCaption, bar, innerCaption) {
-		this.container = container;
-		this.outerCaption = outerCaption;
-		this.bar = bar;
-		this.innerCaption = innerCaption;
-		this.captions = $(innerCaption).add(outerCaption);
+
+function ProgressBar(container, outerCaption, bar, innerCaption) {
+	this.container = container;
+	this.outerCaption = outerCaption;
+	this.bar = bar;
+	this.innerCaption = innerCaption;
+	this.captions = $(innerCaption).add(outerCaption);
+}
+ProgressBar.prototype = {
+	container: $(),
+	outerCaption: $(),
+	bar: $(),
+	innerCaption: $(),
+	captions: $(),
+
+	text: function (newVal) {
+		/// <summary>Gets or sets the caption displayed on the progress bar.</summary>
+		if (arguments.length === 0)
+			return this.outerCaption.text();
+		this.captions.text(newVal);
+	},
+	val: function (newVal, max) {
+		/// <summary>Gets or sets the progress bar's value.</summary>
+		if (arguments.length === 0)
+			return this.bar.width();
+		if (max)
+			newVal = 100 * newVal / max;
+
+		this.bar.width(newVal + '%');
+		if (newVal === 0)
+			this.innerCaption.width(0);
+		else
+			this.innerCaption.width(100 * 100 / newVal + '%');
 	}
-	ProgressBar.prototype = {
-		container: $(),
-		outerCaption: $(),
-		bar: $(),
-		innerCaption: $(),
-		captions: $(),
+};
 
-		text: function (newVal) {
-			if (arguments.length === 0)
-				return this.outerCaption.text();
-			this.captions.text(newVal);
-		},
-		val: function (newVal, max) {
-			if (arguments.length === 0)
-				return this.bar.width();
-			if (max)
-				newVal = 100 * newVal / max;
+jQuery.fn.progressBar = function () {
+	/// <summary>Creates and manages an HTML progress bar.</summary>
+	/// <returns type="ProgressBar" />
+	var container = this;
+	if (!container.length) return null;
 
-			this.bar.width(newVal + '%');
-			if (newVal === 0)
-				this.innerCaption.width(0);
-			else
-				this.innerCaption.width(100 * 100 / newVal + '%');
-		}
-	};
+	if (container.children().length) {
+		var outerCaption = container.children(".OuterText");
+		var bar = container.children(".ProgressBar");
+		var innerCaption = bar.children();
+	} else {
+		container.addClass('ProgressContainer');
+		var outerCaption = $('<span class="OuterText" />').appendTo(container);
+		var bar = $('<div class="ProgressBar" />').appendTo(container);
+		var innerCaption = $('<span />').appendTo(bar);
+	}
 
-	jQuery.fn.progressBar = function () {
-		var container = this;
-		if (!container.length) return null;
-
-		if (container.children().length) {
-			var outerCaption = container.children(".OuterText");
-			var bar = container.children(".ProgressBar");
-			var innerCaption = bar.children();
-		} else {
-			container.addc('ProgressContainer');
-			var outerCaption = container.append('<span class="OuterText" />');
-			var bar = container.append('<div class="ProgressBar" />');
-			var innerCaption = container.append('<span />');
-		}
-
-		return new ProgressBar(container, outerCaption, bar, innerCaption);
-	};
-})(jQuery);
+	return new ProgressBar(container, outerCaption, bar, innerCaption);
+};
