8d36dd0b6477e2281fae6fefbc6838378686a089
SLaks <Schabse@GMail.com>
2010-08-22 20:45:36 -0400

    Azure services stub

---
+++ Source/OcrEngine/Prax.OcrEngine.Services.Azure/AzureProcessorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prax.OcrEngine.Services.Azure {
+	///<summary>Controls document processors on Azure worker nodes.</summary>
+	public class AzureProcessorController : IProcessorController {
+		public void BeginProcessing(DocumentIdentifier id) {
+			throw new NotImplementedException();
+		}
+
+		public void CancelProcessing(DocumentIdentifier id) {
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Source/OcrEngine/Prax.OcrEngine.Services.Azure/AzureStorageClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prax.OcrEngine.Services.Azure {
+	///<summary>An IStorageClient implementation that uses Azure blob storage.</summary>
+	public class AzureStorageClient : IStorageClient {
+		public Guid UploadDocument(Guid userId, string name, System.IO.Stream document, long length) {
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Document> GetDocuments(Guid userId) {
+			throw new NotImplementedException();
+		}
+
+		public Document GetDocument(DocumentIdentifier id) {
+			throw new NotImplementedException();
+		}
+
+		public void DeleteDocument(DocumentIdentifier id) {
+			throw new NotImplementedException();
+		}
+
+		public void SetScanProgress(DocumentIdentifier id, int progress) {
+			throw new NotImplementedException();
+		}
+
+		public void SetState(DocumentIdentifier id, DocumentState state) {
+			throw new NotImplementedException();
+		}
+	}
+}
