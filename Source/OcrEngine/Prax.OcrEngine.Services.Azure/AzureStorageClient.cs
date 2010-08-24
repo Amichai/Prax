f23a45d8b786446a41386807700a98e90ab800c7
SLaks <Schabse@GMail.com>
2010-08-24 12:51:17 -0400

    Remove unnecessary workaround

--- Source/OcrEngine/Prax.OcrEngine.Services.Azure/AzureStorageClient.cs
+++ Source/OcrEngine/Prax.OcrEngine.Services.Azure/AzureStorageClient.cs
@@ -21,29 +21,16 @@ namespace Prax.OcrEngine.Services.Azure {
 			container.CreateIfNotExist();
 		}
 
-		//When uploading a document, there is a race condition
-		//in which GetDocuments is called while a document is 
-		//still uploading.  When that happens, the new blob is
-		//returned missing some or all of the metadata. To fix
-		//this, I ignore blobs that don't have enough metadata
-		//in GetDocuments.  The bug happens against developer 
-		//storage; I don't know if it can happen in production
-
-		///<summary>The number of metadata properties in a basic blob.</summary>
-		///<remarks>Any blobs with less metadata will be ignored.</remarks>
-		const int BaseMetadataCount = 4;
-
 		public Guid UploadDocument(Guid userId, string name, Stream document, long length) {
 			var id = new DocumentIdentifier(userId, Guid.NewGuid());
 			var blob = CreateBlob(id);
 
 			blob.Properties.ContentType = MimeTypes.ForExtension(Path.GetExtension(name));
-			//If you add metadata, make sure to update BaseMetadataCount.
+
 			blob.Metadata.Add("Name", Uri.EscapeDataString(name));
 			blob.Metadata.Add("Date", DateTime.Now.ToString("u", CultureInfo.InvariantCulture));
 			blob.Metadata.Add("Progress", "0");
 			blob.Metadata.Add("State", DocumentState.ScanQueued.ToString());
-			Debug.Assert(BaseMetadataCount == blob.Metadata.Count, "Please update the BaseMetadataCount constant.");
 
 			blob.UploadFromStream(document);
 
@@ -56,7 +43,6 @@ namespace Prax.OcrEngine.Services.Azure {
 				this.blob = blob;
 				base.Length = blob.Properties.Length;
 
-				//If you add metadata, make sure to update BaseMetadataCount.
 				base.DateUploaded = DateTime.Parse(blob.Metadata["Date"], CultureInfo.InvariantCulture);
 				base.Name = Uri.UnescapeDataString(blob.Metadata["Name"]);
 				base.ScanProgress = int.Parse(blob.Metadata["Progress"], CultureInfo.InvariantCulture);
@@ -66,12 +52,10 @@ namespace Prax.OcrEngine.Services.Azure {
 			public override Stream Read() { return blob.OpenRead(); }
 		}
 
-
 		static readonly BlobRequestOptions GetDocsOptions = new BlobRequestOptions { BlobListingDetails = BlobListingDetails.Metadata, UseFlatBlobListing = true };
 		public IEnumerable<Document> GetDocuments(Guid userId) {
 			return client.ListBlobsWithPrefix(container.Name + "/" + userId.ToString(), GetDocsOptions)
 				.OfType<CloudBlob>()
-				//.Where(b => b.Metadata.Count >= BaseMetadataCount)		//Fix race condition with uploading (See above)
 				.Select(b => new BlobDocument(b));
 		}
 
