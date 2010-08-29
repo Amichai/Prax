fcb0d257f0fb3819c8c043d3cb62f65396571813
SLaks <Schabse@GMail.com>
2010-08-29 16:33:27 -0400

    Fixes

--- Source/OcrEngine/Prax.OcrEngine.Services.Azure/AzureScanWorker.cs
+++ Source/OcrEngine/Prax.OcrEngine.Services.Azure/AzureScanWorker.cs
@@ -13,14 +13,12 @@ namespace Prax.OcrEngine.Services.Azure {
 
 		readonly IStorageClient storage;
 		readonly Func<IDocumentProcessor> processorCreator;
-		readonly CloudQueue queue;
+		readonly SingleDeliveryQueueClient queue;
+
 		public AzureScanWorker(IStorageClient storage, Func<IDocumentProcessor> processorCreator, CloudStorageAccount account) {
 			this.storage = storage;
 			this.processorCreator = processorCreator;
-
-			var client = account.CreateCloudQueueClient();
-			queue = client.GetQueueReference("documents");
-			queue.CreateIfNotExist();
+			queue = new SingleDeliveryQueueClient(account, "documents");
 		}
 
 		///<summary>Runs the scan worker.</summary>
@@ -29,29 +27,91 @@ namespace Prax.OcrEngine.Services.Azure {
 			while (true) {
 				var message = queue.GetMessage();
 
-				while (message == null) {
-					Thread.Sleep(TimeSpan.FromSeconds(5));
-					message = queue.GetMessage();
-				}
-
 				var document = storage.GetDocument(Utils.ParseName(message.AsString));
 
-				//Queue handlers must be idempotent.
-				//TODO: Handle race condition
-				if (document.State != DocumentState.ScanQueued) return;
+				if (document.State != DocumentState.ScanQueued)
+					return;		//TODO: Log (How can this happen?)
+
 				storage.SetState(document.Id, DocumentState.Scanning);
 
 				queue.DeleteMessage(message);
 
-				var processor = processorCreator();
-				processor.ProgressChanged += (sender, e) => {
-					ThreadPool.QueueUserWorkItem(delegate {
-						storage.SetScanProgress(document.Id, processor.ProgressPercentage());
-					});
-				};
-				processor.ProcessDocument(document.OpenRead());
-				storage.SetState(document.Id, DocumentState.Scanned);
+				using (var reporter = new ProgressReporter(storage, document.Id)) {
+					var processor = processorCreator();
+					reporter.StartReporter();
+
+					processor.ProgressChanged += (sender, e) => reporter.SetProgress(processor.ProgressPercentage());
+
+					processor.ProcessDocument(document.OpenRead());
+					reporter.StopReporter();
+					storage.SetState(document.Id, DocumentState.Scanned);
+				}
 			}
+		}
+
+		///<summary>Reports scan progress on a separate thread.</summary>
+		///<remarks>Since updating the scan progress involves making network
+		///calls, this class updates the progress on a separate thread.  The
+		///reporter thread is managed internally; all public methods in this
+		///class must be called on the primary thread.</remarks>
+		sealed class ProgressReporter : IDisposable {
+			readonly IStorageClient storage;
+			readonly DocumentIdentifier documentId;
+			public ProgressReporter(IStorageClient storage, DocumentIdentifier documentId) { this.storage = storage; this.documentId = documentId; }
+
+			//TODO: Check cancellation
+
+			readonly AutoResetEvent notifier = new AutoResetEvent(false);
+			volatile int currentProgress;
+
+			int lastProgress = 01;
+			public void SetProgress(int progress) {
+				currentProgress = progress;
+				notifier.Set();
+			}
+
+			//Since the scan will usually take a long time, this isn't a ManualResetEventSlim.
+			readonly ManualResetEvent reporterStoppedEvent = new ManualResetEvent(false);
+
+			///<summary>Launches the reporter thread.</summary>
+			///<remarks>This is an asynchronous method that creates a new thread.</remarks>
+			public void StartReporter() {
+				ThreadPool.QueueUserWorkItem(delegate { RunReporter(); });
+			}
+			///<summary>Stops the reporter thread.</summary>
+			///<remarks>This is a synchronous method that only returns after the reporter thread exits.
+			///By blocking until the reporter stops, I avoid race conditions where the reporter sets the 
+			///progress after the processor thread sets the state to Scanned.</remarks>
+			public void StopReporter() {
+				currentProgress = -1;
+				notifier.Set();
+
+				while (true) {
+					if (reporterStoppedEvent.WaitOne(TimeSpan.FromSeconds(.5)))
+						return;	//If the event was signaled, stop.
+					//If the event was not signaled, set the notifier again.
+					notifier.Set();
+				}
+			}
+
+			///<summary>Runs the reporter listener synchronously.</summary>
+			void RunReporter() {
+				while (true) {
+					notifier.WaitOne();
+					var progress = currentProgress;	//Read the volatile cross-thread field
+
+					if (progress < 0) {
+						reporterStoppedEvent.Set();
+						return;
+					}
+					if (lastProgress == progress) continue;
+					lastProgress = progress;
+
+					storage.SetScanProgress(documentId, progress);
+				}
+			}
+
+			public void Dispose() { notifier.Dispose(); reporterStoppedEvent.Dispose(); }
 		}
 	}
 }
---
+++ Source/OcrEngine/Prax.OcrEngine.Services.Azure/SingleDeliveryQueueClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+using System.Threading;
+using System.Collections.Concurrent;
+
+namespace Prax.OcrEngine.Services.Azure {
+	///<summary>Reads messages from an Azure queue, guaranteeing that each message will only be returned once.</summary>
+	class SingleDeliveryQueueClient {
+		readonly CloudQueue queue;
+		readonly TableServiceContext processedMessagesContext;
+		const string TableName = "ProcessedMessages";
+
+		public SingleDeliveryQueueClient(CloudStorageAccount account, string queueName) {
+			var queueClient = account.CreateCloudQueueClient();
+			queue = queueClient.GetQueueReference(queueName);
+			queue.CreateIfNotExist();
+
+			var tableClient = account.CreateCloudTableClient();
+			tableClient.CreateTableIfNotExist(TableName);
+			processedMessagesContext = tableClient.GetDataServiceContext();
+		}
+
+		class ProcessedMessage : TableServiceEntity {
+			public ProcessedMessage(CloudQueue queue, CloudQueueMessage message) {
+				base.PartitionKey = queue.Name;
+				base.RowKey = message.Id;
+			}
+		}
+
+		readonly ConcurrentDictionary<CloudQueueMessage, ProcessedMessage> messages = new ConcurrentDictionary<CloudQueueMessage, ProcessedMessage>();
+
+		///<summary>Gets the next message in the queue.</summary>
+		///<returns>A non-null queue CloudQueueMessage.</returns>
+		///<remarks>This is a blocking method.  If the queue is empty, the call will never return.</remarks>
+		public CloudQueueMessage GetMessage() {
+			while (true) {
+				var message = queue.GetMessage();
+
+				while (message == null) {
+					Thread.Sleep(TimeSpan.FromSeconds(5));
+					message = queue.GetMessage();
+				}
+
+				var pm = new ProcessedMessage(queue, message);
+				processedMessagesContext.AddObject(TableName, pm);
+				try {
+					processedMessagesContext.SaveChangesWithRetries();
+				} catch { continue; }	//If we failed to save, assume that a different machine already received the message.
+				messages.AddOrUpdate(message, pm, (k, v) => pm);
+
+				return message;
+			}
+		}
+
+		///<summary>Deletes a processed message from the queue.</summary>
+		///<remarks>This is a non-blocking asynchronous method.</remarks>
+		public void DeleteMessage(CloudQueueMessage message) {
+			queue.BeginDeleteMessage(message, r => {
+				queue.EndDeleteMessage(r);
+
+				//I have no idea whether this is necessary. I'm
+				//afraid that the Azure queue might return the 
+				//same message yet again just after deleting it
+				Thread.Sleep(TimeSpan.FromSeconds(8));
+
+				ProcessedMessage pm;
+				messages.TryRemove(message, out pm);
+				processedMessagesContext.DeleteObject(pm);
+			}, null);
+		}
+	}
+}
