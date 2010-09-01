51ba182809137a187ca8cf386cacccb09e46cc34
SLaks <Schabse@GMail.com>
2010-09-01 13:34:45 -0400

    Fix table deletion

--- Source/OcrEngine/Prax.OcrEngine.Services.Azure/SingleDeliveryQueueClient.cs
+++ Source/OcrEngine/Prax.OcrEngine.Services.Azure/SingleDeliveryQueueClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data.Services.Client;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -31,8 +32,6 @@ namespace Prax.OcrEngine.Services.Azure {
 			}
 		}
 
-		readonly ConcurrentDictionary<CloudQueueMessage, ProcessedMessage> messages = new ConcurrentDictionary<CloudQueueMessage, ProcessedMessage>();
-
 		///<summary>Gets the next message in the queue.</summary>
 		///<returns>A non-null queue CloudQueueMessage.</returns>
 		///<remarks>This is a blocking method.  If the queue is empty, the call will never return.</remarks>
@@ -51,13 +50,12 @@ namespace Prax.OcrEngine.Services.Azure {
 				try {
 					dataContext.SaveChanges();
 				} catch (DataServiceRequestException ex) {
+					Debug.Assert(ex.Response.Count() == 1);
 					if (ex.Response.All(o => o.StatusCode == 409))	//HTTP 409 Conflict
 						continue;
 					else
 						throw;
 				}	//If we failed to save, assume that a different machine already received the message.
-
-				messages.AddOrUpdate(message, pm, (k, v) => pm);
 
 				return message;
 			}
@@ -74,12 +72,12 @@ namespace Prax.OcrEngine.Services.Azure {
 				//same message yet again just after deleting it
 				Thread.Sleep(TimeSpan.FromSeconds(8));
 
-				ProcessedMessage pm;
-				messages.TryRemove(message, out pm);
-
 				var dataContext = tableClient.GetDataServiceContext();
-				dataContext.AttachTo(TableName, pm);
+
+				var pm = new ProcessedMessage(queue, message);
+				dataContext.AttachTo(TableName, pm, etag: "*");	//Since entities will never change, it's not worth storing the eTag.
 				dataContext.DeleteObject(pm);
+
 				dataContext.SaveChangesWithRetries();
 			}, null);
 		}
