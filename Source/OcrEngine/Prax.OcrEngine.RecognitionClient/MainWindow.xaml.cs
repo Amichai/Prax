104d0a745c6d86898a99384ca978c2d2c12b372f
SLaks <Schabse@GMail.com>
2010-11-03 16:33:42 -0400

    Windows UI skeleton

---
+++ Source/OcrEngine/Prax.OcrEngine.RecognitionClient/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prax.OcrEngine.RecognitionClient {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private void AddImage_Click(object sender, RoutedEventArgs e) {
+
+		}
+
+		private void CancelAll_Click(object sender, RoutedEventArgs e) {
+
+		}
+	}
+}
