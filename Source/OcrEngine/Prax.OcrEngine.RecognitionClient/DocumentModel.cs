001a0d48762cf8037d8e1a09174910ca9bbb51e6
SLaks <Schabse@GMail.com>
2010-11-03 19:29:09 -0400

    Finish UI

---
+++ Source/OcrEngine/Prax.OcrEngine.RecognitionClient/DocumentModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.IO;
+using Prax.OcrEngine.Services;
+
+namespace Prax.OcrEngine.RecognitionClient {
+	///<summary>The document model class bound to the ListView in the UI.</summary>
+	class DocumentModel : INotifyPropertyChanged {
+		int progress;
+
+		public DocumentModel(string path) {
+			if (path == null) throw new ArgumentNullException("path");
+			FilePath = path;
+			Size = new FileInfo(path).Length;
+		}
+
+		public long Size { get; private set; }
+		public string FilePath { get; private set; }
+
+		///<summary>Gets or sets the scan progress.</summary>
+		public int Progress {
+			get { return progress; }
+			set { progress = value; OnPropertyChanged("Progress"); }
+		}
+
+		DocumentState state;
+		///<summary>Gets or sets the state of the document.</summary>
+		public DocumentState State {
+			get { return state; }
+			set { state = value; OnPropertyChanged("State"); }
+		}
+
+
+		//Databinding properties
+		public string FileName { get { return Path.GetFileName(FilePath); } }
+		public string SizeString { get { return Size.ToSizeString(); } }
+
+		///<summary>Occurs when a property value is changed.</summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+		///<summary>Raises the PropertyChanged event.</summary>
+		///<param name="name">The name of the property that changed.</param>
+		protected virtual void OnPropertyChanged(string name) { OnPropertyChanged(new PropertyChangedEventArgs(name)); }
+		///<summary>Raises the PropertyChanged event.</summary>
+		///<param name="e">An EventArgs object that provides the event data.</param>
+		protected virtual void OnPropertyChanged(PropertyChangedEventArgs e) {
+			if (PropertyChanged != null)
+				PropertyChanged(this, e);
+		}
+	}
+}
--- Source/OcrEngine/Prax.OcrEngine.RecognitionClient/MainWindow.xaml.cs
+++ Source/OcrEngine/Prax.OcrEngine.RecognitionClient/MainWindow.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.IO;
+using Path = System.IO.Path;
+using Prax.OcrEngine.Services;
 
 namespace Prax.OcrEngine.RecognitionClient {
 	/// <summary>
@@ -28,5 +32,23 @@ namespace Prax.OcrEngine.RecognitionClient {
 		private void CancelAll_Click(object sender, RoutedEventArgs e) {
 
 		}
+	}
+	///<summary>Used to provide a list of documents for the XAML designer.</summary>
+	class DummyDocumentList : ObservableCollection<DocumentModel> {
+		static string ImagePath {
+			get {
+				return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), @"Web\Wallpaper");
+			}
+		}
+
+		static readonly DocumentState[] eligibleStates = { DocumentState.ScanQueued, DocumentState.Scanning, DocumentState.Scanned };
+		static readonly Random rand = new Random();
+		public DummyDocumentList()
+			: base(
+				Directory.EnumerateFiles(ImagePath, "*.jpg", SearchOption.AllDirectories)
+				.Select(p => new DocumentModel(p) {
+					Progress = rand.Next(0, 100),
+					State = eligibleStates[rand.Next(eligibleStates.Length)]
+				})) { }
 	}
 }
