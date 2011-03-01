d53f7504fe6040ff1d34c1d77471bc38ab71c414
SLaks <Schabse@GMail.com>
2011-03-01 14:17:17 -0500

    Change RP to class - it doesn't represent a value

--- Source/OcrEngine/Prax.OcrEngine.Engine/PatternRecognition/RecognizedPattern.cs
+++ Source/OcrEngine/Prax.OcrEngine.Engine/PatternRecognition/RecognizedPattern.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace Prax.OcrEngine.Engine.PatternRecognition {
 	///<summary>The result of a pattern recognizer invocation.</summary>
-	public struct RecognizedPattern {
+	[ImmutableObject(true)]
+	public class RecognizedPattern {
 		///<summary>Creates a RecognizedPattern value.</summary>
 		///<param name="label">The label that the data was recognized as.</param>
 		///<param name="certainty">The probability that the recognition is correct, between 0 and 1.</param>
-		public RecognizedPattern(string label, double certainty)
-			: this() {
+		public RecognizedPattern(string label, double certainty) {
 			if (certainty < 0 || certainty > 1)
 				throw new ArgumentOutOfRangeException("certainty", "Certainty must be between 0 and 1.");
 			Label = label;
@@ -21,7 +22,5 @@ namespace Prax.OcrEngine.Engine.PatternRecognition {
 		public string Label { get; private set; }
 		///<summary>Gets the probability that the recognition is correct, between 0 and 1.</summary>
 		public double Certainty { get; private set; }
-
-		//TODO: Equality
 	}
 }
