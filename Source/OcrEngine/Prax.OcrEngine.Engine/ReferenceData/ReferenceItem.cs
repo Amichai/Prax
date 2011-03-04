0f2e2e45e481d3e69916b2e19ea41d6ed837c8ac
SLaks <Schabse@GMail.com>
2011-03-03 19:10:54 -0500

    Add constructor

--- Source/OcrEngine/Prax.OcrEngine.Engine/ReferenceData/ReferenceItem.cs
+++ Source/OcrEngine/Prax.OcrEngine.Engine/ReferenceData/ReferenceItem.cs
@@ -10,6 +10,14 @@ namespace Prax.OcrEngine.Engine.ReferenceData {
 	///<remarks>These objects are very large (especially in aggregate) and should be avoided wherever possible.</remarks>
 	[ImmutableObject(true)]
 	public sealed class ReferenceItem {
+		public ReferenceItem(string label, IList<int> data) {
+			if (label == null) throw new ArgumentNullException("label");
+			if (data == null) throw new ArgumentNullException("data");
+
+			Label = label;
+			Data = new ReadOnlyCollection<int>(data);
+		}
+
 		///<summary>The label describing this item.</summary>
 		public string Label { get; private set; }
 		///<summary>Data corresponding to the item.</summary>
