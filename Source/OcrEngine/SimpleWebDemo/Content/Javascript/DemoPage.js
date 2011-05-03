ea8a754aeb8de6e1e95f9a21737e439ca11b4b70
SLaks <Schabse@GMail.com>
2011-05-03 19:33:36 -0400

    Buttons

---
+++ Source/OcrEngine/SimpleWebDemo/Content/Javascript/DemoPage.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="../../Scripts/jquery-1.5.1-vsdoc.js" />
+/// <reference path="../../Scripts/jquery-ui-1.8.11.js" />
+/// <reference path="../../Scripts/modernizr-1.7.js" />
+/// <reference path="../../Scripts/jquery.uploadify.v2.1.4.js" />
+
+$('button, .Button').button();
+
+$('#upload input:file').uploadify({
+	uploader: basePath + 'Content/uploadify.swf',
+	script: basePath + 'Recoginition/Upload',
+	fileDataName: 'file',
+	auto: true,
+
+	height:$('#upload').outerHeight(),
+	width: $('#upload').outerWidth(),
+	wmode: 'transparent',
+	hideButton: true,
+	queueID: 'I-Dont-Exist' //I don't want their default queue at all
+})
+$('#upload input:file').before("Upload your<br />own image");
