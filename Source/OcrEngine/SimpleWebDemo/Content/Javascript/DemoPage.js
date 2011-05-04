03c5c9ee559d4e2146294a1f8649755a86ff08ba
SLaks <Schabse@GMail.com>
2011-05-03 21:13:03 -0400

    Translation step

--- Source/OcrEngine/SimpleWebDemo/Content/Javascript/DemoPage.js
+++ Source/OcrEngine/SimpleWebDemo/Content/Javascript/DemoPage.js
@@ -2,6 +2,9 @@
 /// <reference path="../../Scripts/jquery-ui-1.8.11.js" />
 /// <reference path="../../Scripts/modernizr-1.7.js" />
 /// <reference path="../../Scripts/jquery.uploadify.v2.1.4.js" />
+/// <reference path="../../Scripts/bbq.js" />
+/// <reference path="../../Scripts/jquery.form.wizard.js" />
+/// <reference path="TextWizard.js" />
 
 $('button, .Button').button();
 
@@ -11,10 +14,12 @@ $('#upload input:file').uploadify({
 	fileDataName: 'file',
 	auto: true,
 
-	height:$('#upload').outerHeight(),
+	height: $('#upload').outerHeight(),
 	width: $('#upload').outerWidth(),
 	wmode: 'transparent',
 	hideButton: true,
 	queueID: 'I-Dont-Exist' //I don't want their default queue at all
-})
+});
 $('#upload input:file').before("Upload your<br />own image");
+
+var textWizard = new TextWizard("#textWizard", "#showTextWizard");
---
+++ Source/OcrEngine/SimpleWebDemo/Content/Javascript/TextWizard.js
@@ -0,0 +1,84 @@
+﻿/// <reference path="../../Scripts/jquery-1.5.1-vsdoc.js" />
+/// <reference path="../../Scripts/jquery-ui-1.8.11.js" />
+/// <reference path="../../Scripts/modernizr-1.7.js" />
+/// <reference path="../../Scripts/jquery.uploadify.v2.1.4.js" />
+/// <reference path="../../Scripts/bbq.js" />
+/// <reference path="../../Scripts/jquery.form.wizard.js" />
+
+
+function TextWizard(container, trigger) {
+	var self = this;
+	trigger = $(trigger);
+
+	this.wizard = $(container)
+		.dialog({
+			autoOpen: false,
+			position: ['center', trigger.position().top + trigger.outerHeight() + 10],
+			resizable: false
+		});
+
+	this.translationStep.setUp(this);
+
+	trigger.click(function () { self.wizard.dialog("open"); });
+}
+
+TextWizard.prototype = {
+	wizard: $(),
+
+	translationStep: {
+		sourceBox: $(),
+		targetBox: $(),
+
+		setUp: function (parent) {
+			var self = this;
+			var translationUpdateTimer = false;
+			this.targetBox = parent.wizard.find('#translatedText');
+			this.sourceBox = parent.wizard.find('#sourceText')
+				.bind('mousemove mousedown mouseup', function (e) {
+					if (e.which === 1) {
+						self.targetBox.width(self.sourceBox.width());
+						$('#translationBranding').width(self.sourceBox.outerWidth());
+						self.targetBox.height(self.sourceBox.height());
+					}
+				})
+				.bind('input propertychange', function () {
+					if (translationUpdateTimer)
+						clearInterval(translationUpdateTimer);
+
+					var text = $.trim(self.sourceBox.val());
+					if (text === self.lastSourceText) return;
+					self.targetBox.addClass("Loading");
+
+					translationUpdateTimer = setTimeout(function () {
+						self.updateTranslation();
+						translationUpdateTimer = false;
+					}, 500);
+				});
+			google.setOnLoadCallback(function () {
+				self.updateTranslation();
+			});
+			google.load("language", "1");
+		},
+
+		lastSourceText: null,
+		updateTranslation: function () {
+			var text = $.trim(this.sourceBox.val());
+			if (text === this.lastSourceText) {
+				this.targetBox.removeClass("Loading");
+				return;
+			}
+
+			var self = this;
+			google.language.translate(
+				{ text: text, type: "text" },
+				"", //Detect source language
+				"ar",
+				function (result) {
+					self.lastSourceText = text;
+					self.targetBox.text(result.translation)
+								  .removeClass("Loading");
+				}
+			);
+		}
+	}
+};
