f6d280fc8508ec81ec45c9f56af6d24cadbbbc8e
SLaks <Schabse@GMail.com>
2011-05-26 13:52:20 -0400

    Text renderer demo

---
+++ Source/TextRenderer/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextRenderer {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+		public MainWindow() {
+			InitializeComponent();
+			SetImage();
+		}
+
+		public void SetImage() {
+			var text = String.IsNullOrWhiteSpace(textBox.Text)?"Enter some text above" : textBox.Text;
+			preview.Source = Renderer.RenderImage(text);
+		}
+
+		private void textBox_TextChanged(object sender, TextChangedEventArgs e) {
+			SetImage();
+		}
+	}
+}
