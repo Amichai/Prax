86810adf7b2b2b06761d5a9acb62c7cc7a320a55
SLaks <Schabse@GMail.com>
2011-06-15 21:47:17 -0400

    Basic measuring

--- Source/TextRenderer/MainWindow.xaml.cs
+++ Source/TextRenderer/MainWindow.xaml.cs
@@ -22,9 +22,18 @@ namespace TextRenderer {
 			SetImage();
 		}
 
+		static readonly BasicTextParagraphProperties format = new BasicTextParagraphProperties("Tahoma", 13, FlowDirection.LeftToRight);
 		public void SetImage() {
-			var text = String.IsNullOrWhiteSpace(textBox.Text)?"Enter some text above" : textBox.Text;
-			preview.Source = Renderer.RenderImage(text);
+			var text = String.IsNullOrWhiteSpace(textBox.Text) ? "Enter some text above" : textBox.Text;
+			//preview.Source = Renderer.RenderImage(text);
+
+			const int width = 300;
+
+			var output = new DrawingGroup();
+			foreach (var line in Measurer.MeasureLines(text, width, format, output)) {
+				line.Dispose();
+			}
+			preview.Source = new DrawingImage(output);
 		}
 
 		private void textBox_TextChanged(object sender, TextChangedEventArgs e) {
