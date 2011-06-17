0755c0cae6bb3e6b511e142a4de5b8800ed5e21e
SLaks <Schabse@GMail.com>
2011-06-17 15:22:34 -0400

    Landing page invocation

--- Source/OcrEngine/SimpleWebDemo/Controllers/HomeController.cs
+++ Source/OcrEngine/SimpleWebDemo/Controllers/HomeController.cs
@@ -7,6 +7,16 @@ using System.Web.Mvc;
 namespace SimpleWebDemo.Controllers {
 	public class HomeController : Controller {
 		public ActionResult Index() {
+			if (Request.Url.DnsSafeHost.StartsWith("demo", StringComparison.OrdinalIgnoreCase))
+				return View("Demo");
+			return View();
+		}
+
+		public ActionResult Demo() {
+			return View();
+		}
+
+		public ActionResult About() {
 			return View();
 		}
 	}
