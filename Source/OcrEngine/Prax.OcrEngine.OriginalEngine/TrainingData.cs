67dff899b6bd39eeee11ec3a490e9cdc3a894c39
Amichai <amichaimlevy@gmail.com>
2011-07-04 17:14:40 -0400

    Implemented the Compare() method within the Engine.Library class. Still needs to be tested.

--- Source/OcrEngine/Prax.OcrEngine.OriginalEngine/TrainingData.cs
+++ Source/OcrEngine/Prax.OcrEngine.OriginalEngine/TrainingData.cs
@@ -57,11 +57,8 @@ namespace Prax.OcrEngine.Engine {
 															= new Dictionary<string, List<HeuristicReturnValues>>();
 		public List<string> ListOfLabels = new List<string>();
 		public List<LookupResult> Compare(HeuristicReturnValues unlabledHeuristic) {
-		
-			throw new NotImplementedException();
-		/*
-				
-			int numberOfUniqueLabels = library.Count();
+			var results = new List<LookupResult>();
+			int numberOfUniqueLabels = ListOfLabels.Count();
 			int sizeOfHeuristicArray = unlabledHeuristic.Count;
 			int numberOfLabelsToCount = numberOfUniqueLabels;
 			double[][] probabilityFromEachHeuristic = new double[numberOfLabelsToCount][];
@@ -75,47 +72,40 @@ namespace Prax.OcrEngine.Engine {
 			}
 
 			for (int heurIdx = 0; heurIdx < sizeOfHeuristicArray; heurIdx++) {
-				for (int inspectionLbl = 0; inspectionLbl < numberOfUniqueLabels; inspectionLbl++)
-						for (int lblTrialIdx = 0; lblTrialIdx < ListOfLabels[inspectionLbl]
-							listOfIndicies[inspectionLbl].Count; lblTrialIdx++){
-							if(unlabledHeuristic.GetAtIndex(heurIdx) == indicies.Value[lblTrial].GetAtIndex(heurIdx)){
-								lblComparisonResults[inspectionLbl][heurIdx]++;
-							}
+				for (int lblIdx = 0; lblIdx < numberOfUniqueLabels; lblIdx++){
+					string currentLabel = ListOfLabels[lblIdx];
+					for (int lblTrialIdx = 0; lblTrialIdx < Content[currentLabel].Count(); lblTrialIdx++){
+						if(unlabledHeuristic.GetAtIndex(heurIdx) == Content[currentLabel][lblTrialIdx].GetAtIndex(heurIdx)){
+							lblComparisonResults[lblIdx][heurIdx]++;
 						}
-						totalComparison_test[inspectionLbl] += lblComparisonResults[inspectionLbl][heurIdx];
 					}
-				for (int labelIndex = 0; labelIndex < numberOfUniqueLabels; labelIndex++)
-				//foreach (int labelIndex in getInspectionLabelIdicies())
-            {
-					lblComparisonResults[labelIndex][heurIdx] = lblComparisonResults[labelIndex][heurIdx] / (double)listOfIndicies[labelIndex].Count;
+					totalComparison_test[lblIdx] += lblComparisonResults[lblIdx][heurIdx];
 				}
-				//This data structure represents:
-			}       //What percentage of the time the current heuristics on the current heuristic array were equal to the heuristics on the training data for every given number
-
+				for (int labelIndex = 0; labelIndex < numberOfUniqueLabels; labelIndex++){
+					string currentLabel = ListOfLabels[labelIndex];
+					lblComparisonResults[labelIndex][heurIdx] = lblComparisonResults[labelIndex][heurIdx] / (double)Content[currentLabel].Count;
+				}
+			}
 			//We are working to produce two DSs: lblComparisonResults[][], totalComparison_test[]
-
 			double heuristicProbabilisticIndication;
 			double multiplicativeOffset;
 			labelProbability = new double[numberOfLabelsToCount];
-			double maxProb = 0;
-			int maxProbIndex = 0;
+			//double maxProb = 0;
+			//int maxProbIndex = 0;
 			double aprioriProb = 1.0 / (double)numberOfLabelsToCount;
 			double factorIncrease = (1.0 - aprioriProb) / aprioriProb;
 
 			for (int inspectionLbl = 0; inspectionLbl < numberOfUniqueLabels; inspectionLbl++)
-			//foreach (int inspectionLbl in getInspectionLabelIdicies())
-        {
+			{
 				labelProbability[inspectionLbl] = 1.0 / (double)numberOfLabelsToCount;
 				for (int heurIdx = 0; heurIdx < sizeOfHeuristicArray; heurIdx++) {
 					double comparisonToThisLabel = lblComparisonResults[inspectionLbl][heurIdx];
 					double comparisonToOtherLabels = 0;
 					for (int comparisonLbl = 0; comparisonLbl < numberOfUniqueLabels; comparisonLbl++)
-					//foreach (int comparisonLbl in getInspectionLabelIdicies())
-                {
+					{
 						if (inspectionLbl != comparisonLbl)
 							comparisonToOtherLabels += lblComparisonResults[comparisonLbl][heurIdx];
 					}
-
 					if (comparisonToThisLabel + comparisonToOtherLabels != 0) {
 						heuristicProbabilisticIndication = comparisonToThisLabel / (comparisonToThisLabel + comparisonToOtherLabels);
 						heuristicsControl.buildHeuristicProbabilityHistorgram(heuristicProbabilisticIndication, inspectionLbl, heurIdx);
@@ -128,17 +118,77 @@ namespace Prax.OcrEngine.Engine {
 							heurIdx = sizeOfHeuristicArray;
 					}
 				}
-				if (labelProbability[inspectionLbl] > maxProb && listOfIndicies[maxProbIndex].Count > 0) {
-					maxProb = labelProbability[inspectionLbl];
-					maxProbIndex = inspectionLbl;
+				string currentLabel = ListOfLabels[inspectionLbl];
+				if (Content[currentLabel].Count > 0) {
+					var result = new LookupResult(currentLabel, labelProbability[inspectionLbl]);
+					results.Add(result);
 				}
 			}
-			if (maxProb > 0) {
-				return new Tuple<string, double>(listOfIndexLabels[maxProbIndex], maxProb);
+			if (results.Count() > 0) {
+				return results;
 			} else {
 				return null;
 			}
-			} */
 		}
+
+		private class variancesForEachHeuristic {
+			public List<List<rollingVarianceCalc>> var2 = new List<List<rollingVarianceCalc>>();
+
+			public variancesForEachHeuristic(int numberOfHeur) {
+				for (int i = 0; i < numberOfHeur; i++)
+					var2.Add(new List<rollingVarianceCalc>());
+			}
+
+			public class rollingVarianceCalc {
+				public int n = 0;
+				double mean = 0,
+						M2 = 0;
+
+				public double calcOnlineVar(double x) {
+					n++;
+					double delta = x - mean;
+					mean = mean + delta / n;
+					M2 = M2 + delta * (x - mean);
+
+					double variance = M2 / (n - 1);
+					return variance;
+				}
+			}
+
+			public double newVarianceValue(double value, int heuristicIdx, int labelIdx) {
+				if (labelIdx >= var2[heuristicIdx].Count) {
+					while (labelIdx >= var2[heuristicIdx].Count)
+						var2[heuristicIdx].Add(new rollingVarianceCalc());
+					return var2[heuristicIdx][labelIdx].calcOnlineVar(value);
+				} else {
+					return var2[heuristicIdx][labelIdx].calcOnlineVar(value);
+				}
+			}
+		}
+
+		private static variancesForEachHeuristic variances; //TODO: Make this its own class so it won't have to be static 
+		private static HeuristicsControlPanel heuristicsControl = new HeuristicsControlPanel();
+	}
+	class HeuristicsControlPanel {
+		private int[] probabilityHistorgram = new int[21];
+		public void buildHeuristicProbabilityHistorgram(double probability, int labelUnderInspection, int heuristicUnderInspection) {
+			if (probability == 1)
+				probabilityHistorgram[20]++;
+			else
+				for (int i = 0; i < 20; i++)
+					if (probability >= 0 * .05 && probability < (i + 1) * .05) {
+						probabilityHistorgram[i]++;
+						i = 20;
+					}
+		}
+
+		//TODO: Improve the lookup of labels
+		//Consider ignoring heuristics that are proven to be useless
+
+		//In order to improve the lookup of labels, expose a variable for the width of the segment so it can be adjusted as words are being resolved
+		//do something similar for segmentation
+
+
+		//Automated training - compare existing coordinates with list of exact coordinates
 	}
 }
