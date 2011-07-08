ff1a855c1c0bfa42b53fa6e5bb595b1389101f7c
Amichai <amichaimlevy@gmail.com>
2011-07-07 21:37:42 -0400

    Set bounds.

--- Source/OcrEngine/Prax.OcrEngine.OriginalEngine/HeuristicGeneration/HeuristicSet.cs
+++ Source/OcrEngine/Prax.OcrEngine.OriginalEngine/HeuristicGeneration/HeuristicSet.cs
@@ -23,6 +23,7 @@ namespace Prax.OcrEngine.Engine.HeuristicGeneration {
 			return heuristics[index];
 		}
 		public void GoThroughBoards(List<MatrixBoard> boards, Rectangle rect) {
+			Bounds = rect;
 			foreach (var board in boards) {
 				AddHeursitics(
 					board.Matrix.ExtractRectangularContentArea(rect));
