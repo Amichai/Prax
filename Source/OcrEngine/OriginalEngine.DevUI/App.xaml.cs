8950fac3b028bd8471500e81d285b69bcd129df9
SLaks <Schabse@GMail.com>
2011-07-10 13:42:59 -0400

    Training Visualizer

---
+++ Source/OcrEngine/OriginalEngine.DevUI/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Prax.OcrEngine.Engine.DevUI {
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application {
+	}
+}
---
+++ Source/OcrEngine/OriginalEngine.DevUI/Controls/HeuristicDisplay.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Prax.OcrEngine.Engine.HeuristicGeneration;
+
+namespace Prax.OcrEngine.Engine.DevUI.Controls {
+	/// <summary>
+	/// Interaction logic for HeuristicDisplay.xaml
+	/// </summary>
+	public partial class HeuristicDisplay : UserControl {
+		public HeuristicDisplay() {
+			InitializeComponent();
+		}
+
+
+		public HeuristicSet Source {
+			get { return (HeuristicSet)GetValue(SourceProperty); }
+			set { SetValue(SourceProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for Source.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty SourceProperty =
+			DependencyProperty.Register("Source", typeof(HeuristicSet), typeof(HeuristicDisplay), new UIPropertyMetadata(null, (s, e) => ((HeuristicDisplay)s).OnSourceChanged()));
+
+
+
+		public IEnumerable<ChartPoint> Points {
+			get { return (IEnumerable<ChartPoint>)GetValue(PointsProperty); }
+			set { SetValue(PointsProperty, value); }
+		}
+
+		// Using a DependencyProperty as the backing store for Points.  This enables animation, styling, binding, etc...
+		public static readonly DependencyProperty PointsProperty =
+			DependencyProperty.Register("Points", typeof(IEnumerable<ChartPoint>), typeof(HeuristicDisplay), new UIPropertyMetadata(null));
+
+
+
+		void OnSourceChanged() {
+			if (Source != null)
+				Points = Source.Heuristics.Select((v, i) => new ChartPoint(i, v)).ToList();
+		}
+	}
+	public class ChartPoint {
+		public ChartPoint(int x, int y) { X = x; Y = y; }
+
+		public int X { get; private set; }
+		public int Y { get; private set; }
+	}
+}
